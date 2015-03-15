c4f02fc819ab0e7094f6554388d257dc356f0610
Happykiller <fabrice.rosito@gmail.com>
2015-03-15 16:58:11 +0100

    first commit

--- include/config.js
+++
@@ -1,2 +0,0 @@
-var prefixTable = "";
-var domaine = "http://10.85.32.5/ODA_LIB_LAB/";
--- include/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'localhost';
-$base = 'db_oda_lib_lab';
-$mdp = 'odaliblabpass01';
-$prefixTable = '';
-?>
